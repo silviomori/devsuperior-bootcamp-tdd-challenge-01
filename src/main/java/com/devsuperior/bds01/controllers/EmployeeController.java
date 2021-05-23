ac7ff1b226f1cfc47ce042b5bda01c70155145d5
Silvio Mori Neto <silviomori@gmail.com>
2021-05-23 08:48:06 -0300

    Solve insert new employee data into the database

--- src/main/java/com/devsuperior/bds01/controllers/EmployeeController.java
+++ src/main/java/com/devsuperior/bds01/controllers/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.bds01.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,9 +9,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds01.dto.EmployeeDTO;
 import com.devsuperior.bds01.services.EmployeeService;
@@ -30,6 +35,14 @@ public class EmployeeController {
 		Pageable pageable = PageRequest.of(pageNumber, size, Direction.fromString(direction), orderBy);
 		Page<EmployeeDTO> page = service.findAll(pageable);
 		return ResponseEntity.ok().body(page);
+	}
+
+	@PostMapping
+	public ResponseEntity<EmployeeDTO> insert(@RequestBody EmployeeDTO dto) {
+		dto = service.insert(dto);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(location).body(dto);
 	}
 
 }
--- src/main/java/com/devsuperior/bds01/services/EmployeeService.java
+++ src/main/java/com/devsuperior/bds01/services/EmployeeService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds01.dto.EmployeeDTO;
 import com.devsuperior.bds01.entities.Employee;
+import com.devsuperior.bds01.repositories.DepartmentRepository;
 import com.devsuperior.bds01.repositories.EmployeeRepository;
 
 @Service
@@ -15,11 +16,21 @@ public class EmployeeService {
 
 	@Autowired
 	private EmployeeRepository repository;
+	@Autowired
+	private DepartmentRepository departmentRepository;
 
 	@Transactional(readOnly = true)
 	public Page<EmployeeDTO> findAll(Pageable pageable) {
 		Page<Employee> pageEntities = repository.findAll(pageable);
 		return pageEntities.map(entity -> new EmployeeDTO(entity));
+	}
+
+	@Transactional
+	public EmployeeDTO insert(EmployeeDTO dto) {
+		Employee entity = new Employee(null, dto.getName(), dto.getEmail(),
+				departmentRepository.getOne(dto.getDepartmentId()));
+		entity = repository.save(entity);
+		return new EmployeeDTO(entity);
 	}
 
 }
